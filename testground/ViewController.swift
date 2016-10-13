4e825bd6bd92f37444bf8835c0d0907e03ee67d5
Yuto Mizutani <i.yuto.curiosity@gmail.com>
2016-10-14 08:08:25 +0900

    Initial Commit

---
+++ testground/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testground
+//
+//  Created by Yuto Mizutani on 2016/10/14.
+//  Copyright © 2016年 Yuto Mizutani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
