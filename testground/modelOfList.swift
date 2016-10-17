07c959ee17af8a42f300bfabc0d835ea51fb1c8b
Yuto Mizutani <i.yuto.curiosity@gmail.com>
2016-10-17 12:52:10 +0900

    delete実装,この後アラートを実装

---
+++ testground/modelOfList.swift
@@ -0,0 +1,13 @@
+//
+//  modelOfList.swift
+//  testground
+//
+//  Created by Yuto Mizutani on 2016/10/17.
+//  Copyright © 2016年 Yuto Mizutani. All rights reserved.
+//
+
+import Foundation
+
+class modelOfList {
+    var items = [String]()
+}
